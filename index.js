29e3f12591ff62be608d10f6b08c58c616e8e882
Scatterbeans <scatterbeaned@gmail.com>
2026-01-19 22:40:33 +0100

    Enhance navigation and loading experience; update reports structure and CSS styles

--- index.js
+++ index.js
@@ -1,82 +1,105 @@
 // @ts-check
-const homeSubPage = ["homePage", "rulesPage", "calendarPage"];
-const result = { "win": 2, "loss": 1, "empty": 0 };
-let activeScene = 0;
+import ReportList from "./reports.json" with { type: "json" };
 
-if (this.location.href.endsWith("pages") || this.location.href.endsWith("pages/") ||
-    this.location.href.endsWith("github.io") || this.location.href.endsWith("github.io/")) {
-    var urlString = this.location.href.endsWith("/") ? `index.html?page=0` : `/index.html?page=0`;
-    this.location.assign(urlString);
+window.onloadstart = () => {
+    var modal = document.getElementById("loadingModal");
+    if (modal) modal.style.display = "block";
 }
-var urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('page')) {
-    var pageParam = urlParams.get('page');
-    if (pageParam) {
-        var pageIndex = parseInt(pageParam);
-        if (!isNaN(pageIndex) && pageIndex >= 0 && pageIndex < homeSubPage.length) {
-            if (pageIndex === 0) navSubPage(pageIndex);
+window.onload = () => {
+    var modal = document.getElementById("loadingModal");
+    if (modal) modal.style.display = "none";
+}
+if (window.location.href.endsWith("pages") || window.location.href.endsWith("pages/")) {
+    var urlString = window.location.href.endsWith("/") ? `index.html?subpage=0` : `/index.html?subpage=0`;
+    window.location.assign(urlString);
+}
+class Nav {
+    static subPages = ["homePage", "rulesPage", "calendarPage"];
+    static mainPages = ["index.html", "rw_reports.html"];
+    static navBtns = ["homePageBtn", "rulesPageBtn", "calendarPageBtn", "repPageBtn"];
+    constructor() {return this;}
+/**
+* @param {string} urlSearch
+*/
+    static catchUrlParams(urlSearch) {
+        var urlParams = new URLSearchParams(urlSearch);
+        for (var paramKey of urlParams.keys()) {
+            switch(paramKey) {
+
+                case'subpage':
+                var pageParam = urlParams.get('subpage');
+                if (pageParam) {
+                    var pageIndex = parseInt(pageParam);
+                    if (!isNaN(pageIndex) && pageIndex >= 0 && pageIndex < this.subPages.length) {
+                        var btn = /** @type {HTMLButtonElement} */(document.getElementById(this.navBtns[pageIndex]));
+                        this.navToSubPage(btn, pageIndex); 
+                    }
+                }
+                break;
+            }
+        }
+    }
+/**
+* @param {string} id
+*/
+    static toggleCollapse(id) {
+        var x = document.getElementById(id);
+        if (!x) return;
+        if (x.className.indexOf("w3-show") == -1) {
+          x.className += " w3-show";
+        } else {
+          x.className = x.className.replace(" w3-show", "");
+        }
+}
+/**
+* @param {HTMLButtonElement} btn
+* @param {number} pageIndex
+*/
+    static navToSubPage(btn, pageIndex) {
+        for (var sub of this.subPages) {
+            var subElem = document.getElementById(sub);
+            if (!subElem) {
+                console.log(`Page not found: ${sub}`);
+                return;
+            }
+            subElem.style.display = "none";
+        }
+        var page = document.getElementById(this.subPages[pageIndex]);
+        if (!page) {
+            console.log(`Page not found: ${this.subPages[pageIndex]}`);
+            return;
+        }
+        page.style.display = "block";
+        this.setSubPageBtnActive(btn);
+}
+/**
+* @param {HTMLButtonElement} btn
+*/
+    static setSubPageBtnActive(btn) {
+        btn.classList.replace("w3-2025-orangeade", "w3-orange");
+        btn.classList.add("w3-text-white");
+        btn.classList.add("w3-hover-text-white");
+        for (var navBtn of this.navBtns) {
+            var b = document.getElementById(navBtn);
+            if (b && b.classList.contains("w3-orange")) {
+                b.classList.replace("w3-orange", "w3-2025-orangeade");
+                b.classList.remove("w3-text-white");
+                b.classList.remove("w3-hover-text-white");
+            }
         }
     }
 }
+Nav.catchUrlParams(window.location.search);
+class ReportController {
+    static years = [2025, 2026]
 /**
- * @param {number} pageIndex
- */
-function navSubPage(pageIndex) {
-    for (var sub of homeSubPage) {
-        var subElem = document.getElementById(sub);
-        if (!subElem) throw new Error("Page not found: " + sub);
-        subElem.style.display = "none";
+* @param {string} year
+*/
+    static switchToYear(year) {
+        var yearElem = document.getElementById(year);
+        if (!yearElem) return;
+        yearElem.style.display = "block";
     }
-    var page = document.getElementById(homeSubPage[pageIndex]);
-    if (!page) throw new Error("Page not found: " + homeSubPage[pageIndex]);
-    page.style.display = "block";
-    setSubPageBtnActive( /** @type {HTMLButtonElement} */ (document.getElementById(homeSubPage[pageIndex] + "Btn")));
-}
-/**
- * @param {string} id
- */
-function toggleCollapse(id) {
-    var x = document.getElementById(id);
-    if (!x) return;
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
+    static loadReportList() {
     }
-  }
-
-/**
- * @param {HTMLButtonElement} btn
- * @param {number} pageIndex
- */
-function subPageNavBtnClick(btn, pageIndex) {
-    navSubPage(pageIndex);
-    setSubPageBtnActive(btn);
-}
-/**
- * @param {string} urlPage
- */
-function urlNavBtnClick(urlPage) {
-    location.assign(`${urlPage}.html`);
-}
-/**
- * @param {HTMLButtonElement} btn
- */
-function setSubPageBtnActive(btn) {
-    btn.classList.replace("w3-2025-orangeade", "w3-orange");
-    btn.classList.add("w3-text-white");
-    btn.classList.add("w3-hover-text-white");
-    var btns = document.body.getElementsByTagName("nav")[0].children;
-    for (var i = 0; i < btns.length; i++) {
-        if (btns[i].id === btn.id) continue;
-        btns[i].classList.replace("w3-orange", "w3-2025-orangeade");
-    }
-}
-/**
- * @param {string} year
- */
-function switchToYear(year) {
-    var yearElem = document.getElementById(year);
-    if (!yearElem) return;
-    yearElem.style.display = "block";
 }
