a3826f230d76ff538833ffe0ca9c789e2f4ef3cc
Scatterbeans <scatterbeaned@gmail.com>
2026-02-11 13:38:47 +0100

    Fixes and more content

--- index.js
+++ index.js
@@ -1,70 +1,84 @@
-// @ts-check
-import ReportList from "./resources/reports.json" with { type: "json" };
+if (window.location.href.endsWith("pages") || window.location.href.endsWith("pages/")) {
+    var queryString = window.location.href.endsWith("/") ? `index.html?sub=0` : `/index.html?sub=0`;
+    window.location.assign(queryString);
+}
 
-window.addEventListener("loadstart", () =>{if (loadingModal)loadingModal.style.display="block"});
-window.addEventListener("load", () =>{if (loadingModal)loadingModal.style.display="none"});
+const subPageMappings = [
+    { id: "homePage", buttons: ["homePage-Btn"] },
+    { id: "rulesPage", buttons: ["rulesPage-Btn", "rulesPage-CBtn"] },
+    { id: "calendarPage", buttons: ["calendarPage-Btn", "calendarPage-CBtn"] }
+];
+const mainPageMappings = [
+    { file: "rw_reports.html", buttons: ["repPage-Btn", "repPage-CBtn"]}
+];
+const state = {
+    _activeSub: 0,
+    set activeSub(value) {
+        if (this._activeSub === value) return;
+        this._activeSub = value;
+
+        syncPageUI(value);
+
+        const newUrl = `?sub=${value}`;
+        window.history.pushState({subpage:value}, "", newUrl);
+    },
+    get activeSub() {
+        return this._activeSub;
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () =>{
+    window.addEventListener("popstate", (ev) => {
+        const index = ev.state.sub ?? 0;
+        state._activeSub = index;
+        syncPageUI(index);
+    });
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const initialPage = parseInt(urlParams.get("sub")) || 0;
+    state._activeSub = initialPage;
+    syncPageUI(initialPage);
+
+    const collapseBtn = document.getElementById("navCollapse-Btn");
+    collapseBtn.addEventListener("click", () => {toggleCollapse("navCollapse");});
+    subPageMappings.forEach((mapping, index) => {
+        mapping.buttons.forEach(btnId => {
+            const btn = document.getElementById(btnId);
+            if (btn) {
+                btn.addEventListener("click", () => {
+                    state.activeSub = index;
+                });
+            }
+        });
+    });
+    mainPageMappings.forEach((mapping) => {
+        mapping.buttons.forEach(btnId => {
+            const btn = document.getElementById(btnId);
+            if (btn) btn.addEventListener("click", () => { window.location.assign(mapping.file); });
+        });
+    });
+    window.addEventListener("load", () =>{document.getElementById("loadingModal").classList.remove("w3-show");});
+});
 
 catchUrlParams(window.location.search);
-if (window.location.href.endsWith("pages") || window.location.href.endsWith("pages/")) {
-    var queryString = window.location.href.endsWith("/") ? `index.html?subpage=0` : `/index.html?subpage=0`;
-    window.location.assign(queryString);
-    activeSub = 0;
-}
-if (window.location.href.endsWith("rw_reports.html")) {
-    const menu = document.getElementById("reportMenu");
-    if (menu) {
-        for (var report of ReportList.reports[2026]) {
-            var elem = document.createElement("li");
-            elem.className = "w3-bar-item w3-button w3-hover-opacity";
-            elem.textContent = 
-            `${report.war_date.day}${report.war_date.month} - ${report.opponent.name} (${report.opponent.tag})`;
-            menu.appendChild(elem);
-            elem.addEventListener("click", () => {
-                var frame = document.getElementById("repFrame");
-                if (frame) frame.setAttribute("src", `${report.file_name}`);
-            });
-        }
-    }
-}
-const nav = document.getElementsByTagName("nav").item(0);
-const navBtns = nav ? nav.querySelectorAll("button") : null;
-const loadingModal = document.getElementById("loadingModal");
-var activeSub = 0;
-if (!navBtns || !loadingModal) throw "Elements missing.";
 
-for (var btn of navBtns) {
-    if (btn == null) throw `Button missing.`;
-    if (btn.id.includes("repPage")) continue;
-    if (btn.id.includes("Page")) btn.addEventListener("click", () => { navToSubPage(btn); });
-    if (btn.id == "collapse-Btn") btn.addEventListener("click", () => { toggleCollapse("navCollapse"); });
-}
-/**
-* @param {string} urlSearch
-*/
 function catchUrlParams(urlSearch) {
-    var urlParams = new URLSearchParams(urlSearch);
+    const urlParams = new URLSearchParams(urlSearch);
     for (var paramKey of urlParams.keys()) {
         switch(paramKey) {
 
-            case'subpage':
-                var pageParam = urlParams.get('subpage');
+            case"sub":
+                var pageParam = urlParams.get("sub");
                 if (pageParam) {
-                    var index = parseInt(pageParam);
-                    if (!isNaN(index) && index >= 0 && index < 3 && navBtns) {
-                        var btn = navBtns[index];
-                        var page = btn.id.includes("Page") ?
-                            document.getElementById(btn.id.split("-")[0]) : null;
-                        if (btn && page) navToSubPage(btn); 
-                        activeSub = index;
+                    const index = parseInt(pageParam);
+                    if (index >= 0 && index < subPageMappings.length) {
+                        state.activeSub = index;
                     }
                 }
-                break;
-            }
+            break;
         }
+    }
 }
-/**
-* @param {string} id
-*/
 function toggleCollapse(id) {
     var collapse = document.getElementById(id);
     if (!collapse) return;
@@ -74,34 +88,29 @@ function toggleCollapse(id) {
         collapse.className = collapse.className.replace(" w3-show", "");
     }
 }
-/**
-* @param {HTMLElement} btn
-*/
-function navToSubPage(btn) {
-    if (!navBtns) return;
-    for (var navBtn of navBtns) {
-        if (navBtn.id.includes("Page")) {
-            var page = document.getElementById(navBtn.id.split("-")[0]);
-            if (page) page.style.display = "none";
+function syncPageUI(pageIndex) {
+    subPageMappings.forEach((page, index) => {
+        const pageElem = document.getElementById(page.id).firstChild;
+        const isActive = index === pageIndex;
+
+        if (pageElem) {
+            pageElem.classList.toggle("w3-show", isActive);
+            pageElem.classList.toggle("w3-hide", !isActive);
         }
-    }
-    var show = document.getElementById(btn.id.split("-")[0]);
-    if (show) show.style.display = "block";
-    setSubPageBtnActive(btn);
+        page.buttons.forEach(btnId => {
+            const btn = document.getElementById(btnId);
+            if (btn) updateActiveBtnStyle(btn, isActive);
+        });
+    });
+    const nav = document.getElementById("navCollapse");
+    if (nav) nav.classList.remove("w3-show");
 }
-/**
-* @param {HTMLElement} btn
-*/
-function setSubPageBtnActive(btn) {
-    btn.classList.replace("w3-2025-orangeade", "w3-orange");
-    btn.classList.add("w3-text-white");
-    btn.classList.add("w3-hover-text-white");
-    if (!navBtns) throw "Missing element(s).";
-    for (var navBtn of navBtns) {
-        if (navBtn && navBtn.classList.contains("w3-orange")) {
-            navBtn.classList.replace("w3-orange", "w3-2025-orangeade");
-            navBtn.classList.remove("w3-text-white");
-            navBtn.classList.remove("w3-hover-text-white");
-        }
+function updateActiveBtnStyle(btn, isActive) {
+    if (isActive) {
+        btn.classList.replace("w3-2025-orangeade", "w3-orange");
+        btn.classList.add("w3-text-white", "w3-hover-text-white", "bean-cornerfold-topright");
+    } else {
+        btn.classList.replace("w3-orange", "w3-2025-orangeade");
+        btn.classList.remove("w3-text-white", "w3-hover-text-white", "bean-cornerfold-topright");
     }
 }
