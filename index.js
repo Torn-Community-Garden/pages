0fa944eae3cfa140ca4daee26c430a518e9fff33
Scatterbeans <scatterbeaned@gmail.com>
2026-02-18 20:27:11 +0100

    Remove redundant page redirection logic from homeState initialization

--- index.js
+++ index.js
@@ -49,19 +49,6 @@ const homeState = {
     return this._activeSub;
   },
 };
-try {
-  if (
-    window.location.href.endsWith("pages") ||
-    window.location.href.endsWith("pages/")
-  ) {
-    var queryString = window.location.href.endsWith("/")
-      ? `home.html?sub=0`
-      : `/home.html?sub=0`;
-    window.location.assign(queryString);
-  }
-} catch (err) {
-  f.LogError(`Error handling page redirection: ${err.message}`);
-}
 
 try {
   document.addEventListener("DOMContentLoaded", () => {
